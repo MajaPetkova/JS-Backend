558e75295f86fce0fe5ce8c6a9ef6571d49e4602
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-28 12:18:23 +0100

    Added pages

--- 01.Intro to node.js/server demo/index.js
+++ 01.Intro to node.js/server demo/index.js
@@ -1,25 +1,61 @@
 const http = require("http");
 
+const homePage = `<h1>Home Page</h1> 
+<p>Welcome to our page</p>`;
+
+const aboutPage = `
+<h1>About Us</h1> 
+<p> Contact Information</p>
+`;
+const defaultPage = `
+<h1>404 Not found</h1> 
+<p>The resource you requested cannot be found</p>
+`;
+
 const server = http.createServer((req, res) => {
-  console.log("req received");
-  console.log(req.method, req.url)
-  const url= new URL(req.url , `http://${req.headers.host}`);
+  //   console.log("req received");
+  console.log(req.method, req.url);
+  const url = new URL(req.url, `http://${req.headers.host}`);
 
-if(url.pathname == "/home"){
-    res.write("<h1>Hello world from home page</h1>");
-    res.end();
-}else if(url.pathname == "/about"){
-    res.write("About Page");
-    res.end();
-}else{
-    res.statusCode = 404;
-    res.write("Page not found")
-}
-
-//   console.log(req.headers)
-
-  res.write("Hello world");
-  res.end();
+  if (url.pathname == "/") {
+    homeController(req, res);
+  } else if (url.pathname == "/about") {
+    aboutController(req, res);
+  } else {
+    defaultController(req, res);
+  }
 });
 
-server.listen(3000)
+function homeController(req, res) {
+  res.write(layout(homePage));
+  res.end();
+}
+
+function aboutController(req, res) {
+  res.write(layout(aboutPage));
+  res.end();
+}
+function defaultController(req, res) {
+  res.statusCode = 404;
+  res.write(layout(defaultPage));
+  res.end();
+}
+function layout(body, title = "Hello World") {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>${title}</title>
+</head>
+<body>
+   <nav>
+     <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+     </ul>
+   </nav>
+${body}
+</body>
+</html>`;
+}
+
+server.listen(3000);
