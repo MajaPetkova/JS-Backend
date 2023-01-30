f48ead0cd1a899c3a64110bb87be46e7931db895
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-30 20:03:45 +0100

    Asynchronous variant

--- 02.Node.js Streams and Utilities/fs-demo.js
+++ 02.Node.js Streams and Utilities/fs-demo.js
@@ -8,8 +8,19 @@ const fs = require("fs");
 //  write files 
 // fs.writeFileSync("./output.txt", "Hello again");
 
-const data= JSON.parse(fs.readFileSync("./data.json"))
-data.price++;
 
-fs.writeFileSync("./data.json", JSON.stringify(data))
-console.log(data)
+//  synchronous
+// const data= JSON.parse(fs.readFileSync("./data.json"))
+// data.price++;
+
+// fs.writeFileSync("./data.json", JSON.stringify(data))
+// console.log(data)
+
+fs.readFile("./data.json",(err, dataAsText) =>{
+    const data= JSON.parse(dataAsText)
+    data.price++;
+
+    fs.writeFile("./data.json", JSON.stringify(data), (err)=>{
+        console.log("Write complete")
+    })
+})
