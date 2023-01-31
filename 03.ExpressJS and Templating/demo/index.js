25f175e93d972816bee70cfefd430f8290d56879
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-01-31 20:55:13 +0100

    Update index.js
    redirect, routing with parameters

--- 03.ExpressJS and Templating/demo/index.js
+++ 03.ExpressJS and Templating/demo/index.js
@@ -7,15 +7,32 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-  res.status(201);
+  res.status(201).json({
+  _id: "62126jgc6",
+  name: "Product 1",
+  price: 56
+  });
   res.send("Article created");
 });
 
 app.get("/create", (req, res)=>{
     res.send('<form method="POST"><input name= "name"/><button>SEND</button></form>')
 });
+app.get("/about",( req, res)=>{
+    res.send("About Page")
+})
 app.get("/catalog", (req, res)=> {
     res.send("Catalog")
+});
+app.get("/contact", (req, res) =>{
+    res.redirect("/about")
+})
+app.get("/catalog/:productId", (req, res)=> {
+    console.log(req.params)
+    res.send(`Product with id ${req.params.productId}`)
+})
+app.get("/catalog/:category/:productId", (req, res)=> {
+    res.send("Product from Category")
 });
 app.get("/catalog/*", (req, res)=> {
     res.send("Product Page")
