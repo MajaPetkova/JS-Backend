a7fe8b41bc98b009c0347fd214283c5111731b92
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-01 10:45:44 +0100

    Conditional statement and loop

--- 03.ExpressJS and Templating/hbs-demo/index.js
+++ 03.ExpressJS and Templating/hbs-demo/index.js
@@ -1,15 +1,33 @@
-const express= require("express") ;
-const app= express();
-const handlebars= require("express-handlebars");
+const express = require("express");
+const app = express();
+const handlebars = require("express-handlebars");
 const hbs = handlebars.create({
-  extname: '.hbs'  
+  extname: ".hbs",
 });
-app.engine(".hbs", hbs.engine );
-app.set('view engine', '.hbs')
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
 
+let visitors = 0;
+const products = [
+  {
+    name: "Product 1",
+    price: 15,
+  },
+  {
+    name: "Product 2",
+    price: 48,
+  },
+  {
+    name: "Product 3",
+    price: 34,
+  },
+];
+app.get("/", (req, res) => {
+  res.locals = {
+    count: visitors++,
+    products
+  };
+  res.render("home", { layout: false });
+});
 
-app.get("/", (req, res)=>{
-    res.render('home', {layout:false})
-})
-
-app.listen(3000, ()=> console.log("App is listening on port 3000"))
+app.listen(3000, () => console.log("App is listening on port 3000"));
