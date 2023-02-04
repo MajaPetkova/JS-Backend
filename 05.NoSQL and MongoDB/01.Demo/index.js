185ae491df0c220ee7b2d79b948d30782c4b159c
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-04 12:29:58 +0100

    Added crud operations, sorting, queries

--- 05.NoSQL and MongoDB/01.Demo/index.js
+++ 05.NoSQL and MongoDB/01.Demo/index.js
@@ -21,8 +21,19 @@ async function start() {
 //     console.log(err.errors)
 //   }
 
-    const data = await Car.find({}); // reading data
+    const data = await Car.find({price : {$gt : 2500, $lt: 4000 } }); // reading data
     console.log(data);
-    data.forEach((c) => c.startEngine());
-    data.forEach((c) => console.log(c.VAT));
+    // data.forEach((c) => c.startEngine());
+    // data.forEach((c) => console.log(c.VAT));
+
+    await Car.findByIdAndUpdate('63de2076a4194c20ebf764e5', {price: 2099}) //update by id
+   
+    console.log(await Car.find({}).select("price")) // queries
+    console.log(await Car.find({}).sort({price: 1}))  // sorting  -1 or 1
+    // const car= await Car.findOne({}); //  findOne
+    //  const car= await Car.findById('63de2076a4194c20ebf764e5');  // findById - reading;
+
+    //  car.price= 1890; // modify data
+    //  car.save();
+    // console.log(car)
 }
