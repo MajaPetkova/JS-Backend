00f3e1c549fc5ed7bfa488b35942af0376cff349
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-04 11:30:42 +0100

    Added validation

--- 05.NoSQL and MongoDB/01.Demo/index.js
+++ 05.NoSQL and MongoDB/01.Demo/index.js
@@ -2,17 +2,25 @@ const mongoose = require("mongoose");
 
 const connectionString = "mongodb://localhost:27017/testdb";
 
-
 const carSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, default: 0 },
+  name: { type: String, required: true, minLength:[3, "Name must be longer then 3 letters"] },
+  price: {
+    type: Number,
+    default: 0,
+    validate: {
+      validator: function (value) {
+        return value >= 0;
+      },
+      message: "Price must be positive number",
+    },
+  },
 });
 
-carSchema.methods.startEngine = function(){
-    console.log(`${this.name} goes vroom!`)
-}
-carSchema.virtual("VAT").get(function(){
-    return this.price * 0.2;
+carSchema.methods.startEngine = function () {
+  console.log(`${this.name} goes vroom!`);
+};
+carSchema.virtual("VAT").get(function () {
+  return this.price * 0.2;
 });
 const Car = mongoose.model("Car", carSchema);
 
@@ -23,15 +31,19 @@ async function start() {
     useNewUrlParser: true,
   });
   console.log("Database connected");
+  try {
+    const car = new Car({
+      name: "O",
+      price: -2500,
+    });
+    await car.save(); // writing data
+  } catch (err) {
+    console.log(err.message);
+    console.log(err.errors)
+  }
 
-//   const car = new Car({
-//     name: "VW Golf 3",
-//     // price: 3200,
-//   });
-//   await car.save(); // writing data
-
-  const data = await Car.find({}); // reading data
-  console.log(data);
-  data.forEach(c=> c.startEngine());
-  data.forEach(c=> console.log(c.VAT));
+  //   const data = await Car.find({}); // reading data
+  //   console.log(data);
+  //   data.forEach((c) => c.startEngine());
+  //   data.forEach((c) => console.log(c.VAT));
 }
