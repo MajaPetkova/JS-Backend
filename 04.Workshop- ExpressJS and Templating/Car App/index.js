30f324d2fba868e5b8552c7d8a82ddb9cdd76528
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-05 09:51:48 +0100

    installed mongoose and connecting to database

--- 04.Workshop- ExpressJS and Templating/Car App/index.js
+++ 04.Workshop- ExpressJS and Templating/Car App/index.js
@@ -5,40 +5,50 @@
 // create layout
 // create data service
 // implement controllers
+// add database connection
+// create Car model
+// upgrade car service to use Car model
+// create Accessory model
 
 const express = require("express");
 const hbs = require("express-handlebars");
-const carsService= require("./services/cars")
+const carsService = require("./services/cars");
+
+const initDb = require("./models/index");
 
 const { about } = require("./controllers/about");
-const  create  = require("./controllers/create");
+const create = require("./controllers/create");
 const { details } = require("./controllers/details");
 const { home } = require("./controllers/home");
 const { notFound } = require("./controllers/notFound");
 const deleteCar = require("./controllers/delete");
-const edit = require("./controllers/edit")
+const edit = require("./controllers/edit");
 
-const app = express();
 
-app.engine("hbs", hbs.create({
-    extname: ".hbs"
-}).engine);
-app.set("view engine", 'hbs');
+start();
+async function start() {
+   await initDb();
 
-app.use(express.urlencoded({extended:true}));
-app.use("/static", express.static("static"));
+  const app = express();
 
-app.use(carsService())
+  app.engine("hbs", hbs.create({ extname: ".hbs", }).engine);
+  app.set("view engine", "hbs");
 
-app.get("/", home);
-app.get("/about", about);
-app.get("/create", create.get);
-app.post("/create", create.post);
-app.get("/details/:id", details);
-app.get("/delete/:id", deleteCar.get);
-app.post("/delete/:id", deleteCar.post);
-app.get("/edit/:id", edit.get);
-app.post("/edit/:id", edit.post);
-app.get("*", notFound);
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/static", express.static("static"));
 
-app.listen(3000, () => console.log("App is listening on port 3000"));
+  app.use(carsService());
+
+  app.get("/", home);
+  app.get("/about", about);
+  app.get("/create", create.get);
+  app.post("/create", create.post);
+  app.get("/details/:id", details);
+  app.get("/delete/:id", deleteCar.get);
+  app.post("/delete/:id", deleteCar.post);
+  app.get("/edit/:id", edit.get);
+  app.post("/edit/:id", edit.post);
+  app.get("*", notFound);
+
+  app.listen(3000, () => console.log("App is listening on port 3000"));
+}
