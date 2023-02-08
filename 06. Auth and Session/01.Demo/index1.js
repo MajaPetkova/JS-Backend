a8f4f648a6098991f0956d9d52d98000075f0b51
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-08 10:31:56 +0100

    Added login and register forms

--- 06. Auth and Session/01.Demo/index1.js
+++ 06. Auth and Session/01.Demo/index1.js
@@ -4,6 +4,16 @@ const expressSession = require("express-session");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
+const users = {
+  "peter": {
+    username:"peter",
+    password: "123",
+  },
+  "john": {
+    username:"john",
+    password: "123",
+  },
+};
 app.use(
   expressSession({
     secret: "keyboard cat",
@@ -26,17 +36,33 @@ app.get("/cats", (req, res) => {
   res.send("Cats");
 });
 app.get("/login", (req, res) => {
-    console.log(req.session.user)
+  console.log(req.session.user);
   res.sendFile(__dirname + "/login.html");
 });
 app.post("/login", (req, res) => {
-  if (req.body.username == "maja" && req.body.password == "123") {
-    console.log("successful login");
-    req.session.user = "Maja";
+  const user = users[req.body.username];
+
+  if (user && req.body.password == user.password) {
+    console.log("successful login " + req.body.username);
+    req.session.user = user;
     res.redirect("/");
+  } else {
+    res.status(401).send("Incorrect username or password");
   }
-//   console.log(req.session);
-//   console.log(req.body);
+    console.log(req.session);
+  //   console.log(req.body);
 });
+app.get("/register", (req, res)=>{
+    res.sendFile(__dirname + "/register.html")
+});
+app.post ("/register", (req, res) =>{
+    const user= {
+        username: req.body.username,
+        password: req.body.password,
+    }
+    users[req.body.username] = user;
+    console.log("successful register " + req.body.username)
+    res.redirect("/")
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
