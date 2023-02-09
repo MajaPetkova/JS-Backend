b367b6d51e2a7ffd1bedf9d74bfa36a898323b87
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-09 13:19:39 +0100

    Added cookie with token

--- 06. Auth and Session/02.Demo 2/index.js
+++ 06. Auth and Session/02.Demo 2/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const hbs = require("express-handlebars");
 const bcrypt = require("bcrypt");
+const cookieParser= require("cookie-parser");
+const jwt= require("jsonwebtoken")
 
 const app = express();
 
+const secret= "mySuperSecret";
 const usersSession = {};
 
+app.use(cookieParser());
 app.use("/static", express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.engine(
@@ -17,11 +21,24 @@ app.engine(
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("home");
+    let token= req.cookies["session"];
+    if(token){
+    jwt.verify(token, secret, (err, decodedToken)=>{
+        if(err){
+          return  res.status(401).send("invalid Token");
+        }
+        res.render("home", {email: decodedToken.email });
+    })
+    }else{
+        res.render("home" , {email : "Guest"})
+    }
 });
+
+
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
 
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
@@ -37,15 +54,21 @@ app.post("/register", async (req, res) => {
   };
   res.redirect("/login");
 });
+
+
 app.get("/login", (req, res) => {
   res.render("login");
 });
+
+
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const isAuthenticated = await bcrypt.compare(password, usersSession[email].password);
 
   if (isAuthenticated) {
+    const token= jwt.sign({email},secret, {expiresIn : "2d"});
 
+    res.cookie("session", token)
     res.redirect("/");
   } else {
     res.status(401).send("Wrong username or password");
