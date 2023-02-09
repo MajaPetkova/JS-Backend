4c5d2dfb7cd1a1d7619fe7b3e51a1b2af6485cd0
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-09 12:52:47 +0100

    hashed password and validate password with bcrypt

--- 06. Auth and Session/02.Demo 2/index.js
+++ 06. Auth and Session/02.Demo 2/index.js
@@ -1,28 +1,55 @@
-const express= require("express");
-const hbs= require("express-handlebars");
+const express = require("express");
+const hbs = require("express-handlebars");
+const bcrypt = require("bcrypt");
 
-const app= express();
+const app = express();
 
-app.use(express.urlencoded({extended:true}));
-app.engine("hbs", hbs.engine({
-    extname: "hbs"
-}))
-app.set("view engine","hbs" )
+const usersSession = {};
 
+app.use("/static", express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.engine(
+  "hbs",
+  hbs.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 
-app.get("/", (req, res)=>{
-    res.render("home")
-})
-app.get("/register", (req, res)=>{
-    res.render("register")
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.post("/register", (req, res)=>{
-    console.log(req.body)
-    res.redirect("/")
-})
-app.get("/login", (req, res)=>{
-    res.render("login")
-})
+app.post("/register", async (req, res) => {
+  const { email, password } = req.body;
+  if (usersSession[email]) {
+    res.status(400).send("User already exists");
+  }
 
-app.listen(5000, ()=> console.log("Server is listening on port 5000"))
+  const hash = await bcrypt.hash(password, 10);
+
+  usersSession[email] = {
+    email,
+    password: hash,
+  };
+  res.redirect("/login");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const isAuthenticated = await bcrypt.compare(password, usersSession[email].password);
+
+  if (isAuthenticated) {
+
+    res.redirect("/");
+  } else {
+    res.status(401).send("Wrong username or password");
+  }
+});
+
+app.listen(5000, () => console.log("Server is listening on port 5000"));
