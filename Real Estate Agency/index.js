2a09b1db8c7deb474495425ea750f7a08c383091
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-18 10:53:30 +0100

    initialize app, added libraries and added view engine

---
+++ Real Estate Agency/index.js
@@ -0,0 +1,19 @@
+const express= require("express");
+const {create: handlebars} = require("express-handlebars");
+const session = require("express-session");
+
+
+start()
+async function start(){
+
+    const app = express();
+    app.engine(".hbs", handlebars({
+      extname: ".hbs"
+    }).engine);
+    app.set("view engine", ".hbs")
+    app.use("/static", express.static("static"))
+    app.get("/", (req, res)=>{
+        res.render("home", {layout: false})
+    })
+    app.listen("5000" , ()=>console.log("Server is listening on port 5000"))
+}
