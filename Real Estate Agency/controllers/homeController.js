7067ab69a1371674199b5fe58e6c558504d65a28
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-22 11:42:52 +0100

    visualizing buttons depends on owner and user

--- Real Estate Agency/controllers/homeController.js
+++ Real Estate Agency/controllers/homeController.js
@@ -2,20 +2,24 @@ const router = require("express").Router();
 const preload = require("../middleware/preload");
 const { getAllHousings } = require("../services/housingService");
 
-
 router.get("/", async (req, res) => {
-    const housings =  (await getAllHousings()).sort((a,b)=> a.year - b.year)
+  const housings = (await getAllHousings()).sort((a, b) => a.year - b.year);
   res.render("home", { title: "Home Page", housings });
 });
 
-router.get("/catalog", async(req, res) => {
-   const housings = await getAllHousings()
-  res.render("catalog", { title: "Catalog",  housings });
+router.get("/catalog", async (req, res) => {
+  const housings = await getAllHousings();
+  res.render("catalog", { title: "Catalog", housings });
 });
 
 router.get("/details/:id", preload(true), (req, res) => {
-    console.log(res.locals.housing)
-    res.render("details", { title: "Details Page"});
+  if (req.session.user) {
+    res.locals.housing.hasUser = true;
+    res.locals.housing.isOwner = req.session.user._id == res.locals.housing.owner._id;
+}
+
+  console.log(res.locals.housing);
+  res.render("details", { title: "Details Page"});
 });
 
 module.exports = router;
