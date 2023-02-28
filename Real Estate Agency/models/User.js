b29aca8ceaa058cdecfaaf464cc98aa75d538caa
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-02-28 11:07:45 +0100

    Added validations for the user

--- Real Estate Agency/models/User.js
+++ Real Estate Agency/models/User.js
@@ -1,8 +1,15 @@
 const { Schema, model } = require("mongoose");
+ const NAME_PATTERN= /([A-Za-z]+)\s([A-Za-z]+)/gm;
+
 
 const userSchema = new Schema({
-  name:{type: String, required: true},
-  username: { type: String, required: true },
+  name:{type: String, required: true, validate: {
+    validator(value){
+      return NAME_PATTERN.test(value)
+    },
+    message: "Name must contains First and Last name"
+  }},
+  username: { type: String, required: true,minLength:[5, "Username must be 5 letters long"]  },
   hashedPassword: { type: String, required: true },
 });
 userSchema.index(
