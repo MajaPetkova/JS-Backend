bd5587073f9c4871ebd3198b2cb5ea9e1622cc1e
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-01 23:23:27 +0100

    Added validation for the auction model

--- Auction house/models/Auction.js
+++ Auction house/models/Auction.js
@@ -4,12 +4,21 @@ const {
   Types: { ObjectId },
 } = require("mongoose");
 
+
+const URL_PATTERN = /https?:\/\/(.+)/;
+
 const auctionSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  image: { type: String, required: true },
-  price: { type: Number, required: true },
+  title: { type: String, required: true, minLength:[4, "Title must be at least 4 characters long"]  },
+  description: { type: String, required: true , maxLength:[200, "Description must be not longer than 200 characters"]},
+  category: { type: String, required: true, enum:["vehicles", "estate", "electronics", "furniture", "other"]},
+  image: { type: String, required: true, validate:{
+    validator (value){
+      return URL_PATTERN.test(value)
+    },
+    message: "Image must be valid url"
+     }
+   },
+  price: { type: Number, required: true, min: [0, "Can not be negative number"] },
   owner: { type: ObjectId, ref: 'User', required: true },
   bidder: { type: [ObjectId], ref: "User", default: [] },
 });
