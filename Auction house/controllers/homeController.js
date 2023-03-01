595be6dedba5536f888d35fb3fd49d38838afd1a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-01 17:14:58 +0100

    Added catalog page, partial and update controller

--- Auction house/controllers/homeController.js
+++ Auction house/controllers/homeController.js
@@ -1,8 +1,16 @@
+const { getAllAuctions } = require("../services/auctionService");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
   // console.log(req.session)
   res.render("home", { title: "Home page" });
+});
+
+router.get("/catalog", async(req, res) => {
+  const auctions= await getAllAuctions();
+  console.log(auctions)
+  res.render("browse", { title: "Catalog page", auctions });
 });
 
 module.exports = router;
--- Auction house/services/auctionService.js
+++ Auction house/services/auctionService.js
@@ -1,5 +1,8 @@
 const Auction  = require("../models/Auction");
 
+async function getAllAuctions(){
+    return Auction.find({}).lean();
+}
 async function getAuctionById(id){
     return Auction.findById(id)
 }
@@ -7,8 +10,8 @@ async function createAuction (auction){
     const result= new Auction(auction);
     await result.save();
 }
-
 module.exports= {
+    getAllAuctions,
     getAuctionById,
     createAuction
 };
