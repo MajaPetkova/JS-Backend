eac60fa30d86f2f133e23a101b100df69b2f4d03
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-03-01 15:43:35 +0100

    Added 404 page

--- Auction house/config/routes.js
+++ Auction house/config/routes.js
@@ -5,5 +5,9 @@ const homeController = require("../controllers/homeController")
 module.exports= (app)=>{
     app.use(authController);
     app.use(auctionController);
-    app.use(homeController)
+    app.use(homeController);
+    
+    app.get("*", (req, res)=>{
+        res.render("404", {title: "Page not found"})
+    })
 }
