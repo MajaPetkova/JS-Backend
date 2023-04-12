9758ccc7de3e46fa60f9b6901c2b4f81ef2c599a
Maya Petkova <97319041+MajaPetkova@users.noreply.github.com>
2023-04-12 23:21:14 +0200

    Add catalog page (template and get data)

--- Jobs Add/controllers/homeController.js
+++ Jobs Add/controllers/homeController.js
@@ -1,7 +1,14 @@
+const { getAllAds } = require("../services/adService");
+
 const router = require("express").Router();
-router.get("/", (req, res)=>{
-    console.log(req.session)
-    res.render("home")
+
+router.get("/", (req, res) => {
+  console.log(req.session);
+  res.render("home");
 });
 
+router.get("/catalog", async(req, res) => {
+ const ads= await getAllAds()
+  res.render("catalog", { title: "Catalog Page", ads});
+});
 module.exports = router;
--- Jobs Add/services/adService.js
+++ Jobs Add/services/adService.js
@@ -1,11 +1,14 @@
 const Ad = require("../models/Ad")
 
-
+async function getAllAds(){
+return Ad.find({}).lean()
+}
 async function createAd (ad){
  const result= new Ad(ad)
 await result.save()
 }
 
 module.exports= {
+    getAllAds,
     createAd
 }
