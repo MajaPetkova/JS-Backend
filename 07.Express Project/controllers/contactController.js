3ac1ecb4054101f8d9fc8bc47c0e672a8afa30dd
Maja Petkova <majapetkova1@yahoo.com>
2023-06-29 23:04:32 +0200

    Add error handling middleware, constants and update create contact in controller

--- 07.Express Project/controllers/contactController.js
+++ 07.Express Project/controllers/contactController.js
@@ -11,7 +11,13 @@ const getContacts = (req, res) => {
 // access public
 
 const createContact = (req, res) => {
-  res.status(200).send("Create Contact");
+    console.log(req.body)
+    const {name, email, phone} = req.body;
+    if(!name || !email || !phone){
+        res.status(400)
+        throw new Error("All fields are mandatory")
+    }
+  res.status(201).json({Message: "Create Contact"});
 };
 
 // desc Get Contact
--- 07.Express Project/routes/contactRoutes.js
+++ 07.Express Project/routes/contactRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 
 
 
-router.route("/").get(getContacts);
+router.route("/").get(getContacts)
 router.route("/").post(createContact);
+
 router.route("/:id").get(getContact);
 router.route("/:id").put(updateContact);
 router.route("/:id").delete(deleteContact);
