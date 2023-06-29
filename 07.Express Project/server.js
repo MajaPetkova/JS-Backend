657641ad4368a3c0508dca262228ee7b88ed33c0
Maja Petkova <majapetkova1@yahoo.com>
2023-06-29 21:22:09 +0200

    Initialize new project and install express, nodemon (dev) and dotenv

---
+++ 07.Express Project/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+
+const app= express();
+const port = ( process.env.PORT ) || 4000;
+
+app.get("/", (req, res)=>{
+    res.send("home")
+})
+
+app.listen(port, ()=>{
+    console.log(`App is listening on port ${port}`)
+})
