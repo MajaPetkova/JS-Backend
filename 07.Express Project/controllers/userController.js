ff4a5ec06e46b87f93dc275a2e9d4ac100743ffc
Maja Petkova <majapetkova1@yahoo.com>
2023-07-02 11:45:42 +0200

    Add JWT access token and user login

--- 07.Express Project/controllers/userController.js
+++ 07.Express Project/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 // desc Register a user
@@ -30,17 +31,38 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (user) {
     res.status(201).json({ _id: user._id, email: user.email });
-  }else{
+  } else {
     res.status(400);
-    throw new Error("User data is not valid ")
+    throw new Error("User data is not valid ");
   }
-  res.json({message: "Added new User"});
+  res.json({ message: "Added new User" });
 });
 
 // desc Login a user
 // route POST /api/users/login
 // access public
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const user = await User.findOne({ email });
+  // console.log(user)
+  // compare password with hashedPassword
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign({
+      user: {
+        username:user.username,
+        email: user.email,
+        id: user.id
+      }, 
+    }, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "1m"});
+    res.status(200).json({ accessToken });
+  }else{
+    res.status(401);
+    throw new Error("Email or Password is not valid")
+  }
   res.json({ message: "Login user" });
 });
 
