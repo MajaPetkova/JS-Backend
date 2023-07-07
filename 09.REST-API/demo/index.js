ef1809c26812144f311b394b8dde7725a5cd3a84
Maja Petkova <majapetkova1@yahoo.com>
2023-07-07 11:43:48 +0200

    Add routing for post, put and delete

--- 09.REST-API/demo/index.js
+++ 09.REST-API/demo/index.js
@@ -15,6 +15,7 @@ const data = [
   },
 ];
 const app = express();
+app.use(express.json());
 
 app.get("/catalog", (req, res) => {
   res.json({ data });
@@ -22,14 +23,49 @@ app.get("/catalog", (req, res) => {
 
 app.get("/catalog/:id", (req, res) => {
   const id = req.params.id;
-  const item = data.find(i => i.id == id);
-  if(item){
-    res.json(item)
-  }else{
-    res.status(404).json({error:"Resource not found"})
+  const item = data.find((i) => i.id == id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ error: "Resource not found" });
   }
   res.json(item);
 });
+
+app.post("/catalog", (req, res) => {
+  const item = req.body;
+  item.id = 1000 + (Math.random() * 8999 | 0);
+
+  data.push(item);
+  res.status(201).json(item)
+});
+
+app.put("/catalog/:id", (req,res)=>{
+    const id = req.params.id;
+    const index = data.findIndex((i) => i.id == id);
+
+    const item= req.body;   
+    item.id= id;
+
+    if (index != -1) {
+     data.splice(index, 1, req.body);
+     res.json(req.body)
+    } else {
+      res.status(404).json({ error: "Resource not found" });
+    }
+})
+app.delete("/catalog/:id", (req,res)=>{
+    const id = req.params.id;
+    const index = data.findIndex((i) => i.id == id);
+
+    
+    if (index != -1) {
+     data.splice(index, 1);
+     res.json(req.body)
+    } else {
+      res.status(404).json({ error: "Resource not found" });
+    }
+})
 
 app.listen(5000, () => {
   console.log("App is listening on port 5000");
