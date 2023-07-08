e002929c5cf54baa170f59b52c1008265e2eab92
Maja Petkova <majapetkova1@yahoo.com>
2023-07-08 18:22:37 +0200

    Create furniture service and update furniture controller

--- 09.REST-API/server/src/controllers/furniture.js
+++ 09.REST-API/server/src/controllers/furniture.js
@@ -1,12 +1,72 @@
 const router = require("express").Router();
+const api = require("../services/furnitureService");
 
-router.get("/", (req, res) => {
-  res.json([]);
+router.get("/", async (req, res) => {
+  res.json(await api.getAll());
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body)
-  req.end()
+router.post("/", async (req, res) => {
+  const item = {
+    make: req.body.make,
+    model: req.body.model,
+    year: req.body.year,
+    description: req.body.description,
+    price: req.body.price,
+    img: req.body.img,
+    material: req.body.material,
+  };
+  try {
+    const result = await api.create(item);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ message: "Request Error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  const item = await api.getById(id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ message: `Item with ${id} id is not found!` });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const item = {
+    make: req.body.make,
+    model: req.body.model,
+    year: req.body.year,
+    description: req.body.description,
+    price: req.body.price,
+    img: req.body.img,
+    material: req.body.material,
+  };
+  try {
+    const result = await api.updateById(id, item);
+    res.json(result);
+  } catch (err) {
+    if (err._notFound) {
+      res.status(404).json({ message: `Item with ${id} id is not found!` });
+    } else {
+      console.error(err);
+      res.status(400).json({ message: "Request Error" });
+    }
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await api.deleteById(id);
+    res.json(result);
+  } catch (err) {
+    console.error(err);
+    res.status(404).json({ message: `Item ${id} not found` });
+  }
 });
 
 module.exports = router;
---
+++ 09.REST-API/server/src/services/furnitureService.js
@@ -0,0 +1,54 @@
+const Item = require("../model/Item");
+
+async function getAll() {
+  return Item.find({});
+}
+async function create(item) {
+  const result = new Item({
+    make: item.make,
+    model: item.model,
+    year: item.year,
+    description: item.description,
+    price: item.price,
+    img: item.img,
+    material: item.material,
+  });
+  await result.save();
+  return result;
+}
+
+async function getById(id) {
+  return Item.findById(id);
+}
+
+async function updateById(id, item) {
+  const existing = await Item.findById(id);
+  if (existing) {
+    existing.make = item.make;
+    existing.model = item.model;
+    existing.year = item.year;
+    existing.description = item.description;
+    existing.price = item.price;
+    existing.img = item.img;
+    existing.material = item.material;
+
+    await existing.save();
+    return existing;
+  } else {
+    const error= new Error("Not Found");
+    error._notFound = true;
+    throw error;
+  }
+}
+
+async function deleteById(id) {
+    return await Item.findByIdAndDelete(id);
+}
+
+module.exports = {
+  getAll,
+  create,
+  getById,
+  updateById,
+  deleteById
+};
