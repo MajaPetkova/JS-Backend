55e9de34a2ee9ed6fecccd0ead393057fe0ad171
Maja Petkova <majapetkova1@yahoo.com>
2024-07-21 23:21:12 +0200

    read all products and single product (api)

--- 10.Simple-crud-app/index.js
+++ 10.Simple-crud-app/index.js
@@ -9,6 +9,26 @@ app.get("/", (req, res) => {
   res.send("Hello from node API");
 });
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+    console.log(err);
+  }
+});
+
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
