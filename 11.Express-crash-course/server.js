d8b866c6ee0f1763c4f84fcb04a31c5fa5bb6635
Maja Petkova <majapetkova1@yahoo.com>
2024-07-30 12:37:50 +0200

    Add colors library & add to logger.js, add middleware for all error

--- 11.Express-crash-course/server.js
+++ 11.Express-crash-course/server.js
@@ -3,6 +3,7 @@ import path from "path";
 import posts from "./routes/posts.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
 
 const port = process.env.PORT || 8000;
 
@@ -13,15 +14,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //logger middleware
-app.use(logger)
+app.use(logger);
 
 //setup static folder
 // app.use(express.static(path.join(__dirname, "public")))
 
 //Routes
 app.use("/api/posts", posts);
+
+
 //error handler
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
